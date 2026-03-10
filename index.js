d23965228d01638450dcaaaa5260b1d11ab049be
allinuxuser <165105879+allinuxuser@users.noreply.github.com>
2026-03-10 14:57:30 +0800

    Add the ability to change grid size

--- index.js
+++ index.js
@@ -2,28 +2,76 @@ const container = document.querySelector("#container");
 const body = document.querySelector("body");
 let grids = 0;
 let gridLineContainer = 0
+let gridNumber = 16;
+
+const button = document.createElement("button");
+button.textContent = "Click here to change grid size!"
+container.appendChild(button);
 
 
-for (let i = 0; i <= 16; i++) {
+let count = 0;
+let count2 = 0;
+
+while (count != gridNumber) {
   gridLineContainer = document.createElement("div");
   gridLineContainer.classList.add("grid-line-container");
   gridLineContainer.style.cssText = "display: flex;"
   container.appendChild(gridLineContainer);
-
-  for (let x = 0; x <= 16; x++) {
+  count++;
+  count2 = 0;
+  while (count2 != gridNumber) {
     grids = document.createElement("div");
     grids.classList.add("grid");
     grids.style.cssText = "height: 50px; width: 50px; border: 2px solid black;";
     gridLineContainer.appendChild(grids);
+    count2++
     }
- }
+
+}
+
+
+button.addEventListener("click", () => {
+  container.innerHTML = ''
+  let prompter = prompt("enter num here");
+  gridNumber = prompter;
+  console.log(gridNumber)
+  count = 0;
+  while (count != gridNumber) {
+    gridLineContainer = document.createElement("div");
+    gridLineContainer.classList.add("grid-line-container");
+    gridLineContainer.style.cssText = "display: flex;"
+    container.appendChild(gridLineContainer);
+    count++;
+    count2 = 0;
+    while (count2 != gridNumber) {
+      grids = document.createElement("div");
+      grids.classList.add("grid");
+      grids.style.cssText = "height: 50px; width: 50px; border: 2px solid black;";
+      gridLineContainer.appendChild(grids);
+      count2++
+      }
+
+  }
+
+  griddy = document.querySelectorAll(".grid")
+
+
+  griddy.forEach(grid => {
+    grid.addEventListener('mouseover', event => {
+      event.target.style.backgroundColor = "black";
+    });
+});
+
+
+});
+
 
 griddy = document.querySelectorAll(".grid")
 
 
 
 griddy.forEach(grid => {
-  grid.addEventListener('click', event => {
+  grid.addEventListener('mouseover', event => {
     event.target.style.backgroundColor = "black";
   });
 });
