ccefc3ca5fcf75fdf9182bc6314c3239e1b170eb
allinuxuser <165105879+allinuxuser@users.noreply.github.com>
2026-03-10 15:16:04 +0800

    Optimize the code by putting stuff into functions

--- index.js
+++ index.js
@@ -8,34 +8,9 @@ const button = document.createElement("button");
 button.textContent = "Click here to change grid size!"
 container.appendChild(button);
 
-
-let count = 0;
-let count2 = 0;
-
-while (count != gridNumber) {
-  gridLineContainer = document.createElement("div");
-  gridLineContainer.classList.add("grid-line-container");
-  gridLineContainer.style.cssText = "display: flex;"
-  container.appendChild(gridLineContainer);
-  count++;
-  count2 = 0;
-  while (count2 != gridNumber) {
-    grids = document.createElement("div");
-    grids.classList.add("grid");
-    grids.style.cssText = "height: 50px; width: 50px; border: 2px solid black;";
-    gridLineContainer.appendChild(grids);
-    count2++
-    }
-
-}
-
-
-button.addEventListener("click", () => {
-  container.innerHTML = ''
-  let prompter = prompt("enter num here");
-  gridNumber = prompter;
-  console.log(gridNumber)
-  count = 0;
+function createGrid() {
+  let count = 0;
+  let count2 = 0;
   while (count != gridNumber) {
     gridLineContainer = document.createElement("div");
     gridLineContainer.classList.add("grid-line-container");
@@ -50,29 +25,30 @@ button.addEventListener("click", () => {
       gridLineContainer.appendChild(grids);
       count2++
       }
-
   }
+}
 
+function mouseoverEtch() {
   griddy = document.querySelectorAll(".grid")
-
-
   griddy.forEach(grid => {
     grid.addEventListener('mouseover', event => {
       event.target.style.backgroundColor = "black";
     });
-});
-
-
-});
-
-
-griddy = document.querySelectorAll(".grid")
-
-
-
-griddy.forEach(grid => {
-  grid.addEventListener('mouseover', event => {
-    event.target.style.backgroundColor = "black";
   });
+}
+
+createGrid()
+
+button.addEventListener("click", () => {
+  container.innerHTML = ''
+  let prompter = prompt("enter num here");
+  gridNumber = prompter;
+  console.log(gridNumber)
+  count = 0;
+  container.appendChild(button);
+  createGrid()
+  mouseoverEtch()
 });
 
+
+mouseoverEtch()
